9762a8bb2941065a5f2456f78927e00b15f04574
AMOS KIPKORIR <amos.kipkorir@student.moringaschool.com>
2025-09-15 15:21:22 +0300

    Complete Flask API implementation - all tests passing

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def pytest_configure():
+    from app import app, db
+    from models import Message
+    with app.app_context():
+        db.create_all()
+        # Add test data for PATCH tests
+        test_message = Message(body="Test message", username="Test user")
+        db.session.add(test_message)
+        db.session.commit()
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
